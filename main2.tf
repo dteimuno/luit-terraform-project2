508f767ccf94b9555c65d2584f86aa6672d40067
Dennis Tei-Muno <munodennis@gmail.com>
2025-02-08 22:06:28 -0600

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -2,38 +2,149 @@ provider "aws" {
   region = "us-east-1"
 
 }
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+}
+
+#internet gateway attached to the VPC
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+#  Public Route Table 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Route for outbound traffic to the internet via the Internet Gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+#  Associate Public Route Table with Public Subnets in us-east-1a and us-east-1b
+resource "aws_route_table_association" "public_rt_assoc_az1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_az2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#  Private Route Table for private subnet1 
+resource "aws_route_table" "private_rt_az1" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Route for outbound traffic to the internet via NAT Gateway in AZ-1
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_az1.id
+  }
+}
+
+#  Private Route Table for private subnet2 
+resource "aws_route_table" "private_rt_az2" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Route for outbound traffic to the internet via NAT Gateway in AZ-2
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_az2.id
+  }
+}
+
+#  Associate Private Route Tables with Private Subnets in AZ-1 and AZ-2
+resource "aws_route_table_association" "private_rt_assoc_az1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_az1.id
+}
+
+resource "aws_route_table_association" "private_rt_assoc_az2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt_az2.id
+}
+
+
+#Create NAT gateway in the public subnet
+#  Elastic IP for NAT Gateway in AZ-1
+resource "aws_eip" "nat_eip_az1" {
+}
+
+#  Elastic IP for NAT Gateway in AZ-2
+resource "aws_eip" "nat_eip_az2" {
+}
+
+#  NAT Gateway for AZ-1
+resource "aws_nat_gateway" "nat_az1" {
+  allocation_id = aws_eip.nat_eip_az1.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+}
+
+#  NAT Gateway for AZ-2
+resource "aws_nat_gateway" "nat_az2" {
+  allocation_id = aws_eip.nat_eip_az2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+}
+
 
 #Create a security group that allows HTTP, HTTPS, SSH inbound traffic and all outbound traffic
 resource "aws_security_group" "my-luit-sg" {
   name        = "my-luit-sg"
   description = "Allow all inbound HTTP, HTTPS, and SSH traffic"
-  vpc_id = var.default-vpc_id # default-vpc_id is defined in variables.tf
+  vpc_id      = aws_vpc.my_vpc.id
 }
 resource "aws_security_group_rule" "asg-ingress-http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.my-luit-sg.id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.my-luit-sg.id
   source_security_group_id = aws_security_group.luit-alb-sg.id
 }
 
 #rules for the asg security group
 resource "aws_security_group_rule" "asg-ingress-https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.my-luit-sg.id
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.my-luit-sg.id
   source_security_group_id = aws_security_group.luit-alb-sg.id
 }
 
 resource "aws_security_group_rule" "asg-ingress-ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = aws_security_group.my-luit-sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.my-luit-sg.id
   source_security_group_id = aws_security_group.luit-alb-sg.id
 }
 
@@ -42,7 +153,7 @@ resource "aws_security_group_rule" "asg-egress-all" {
   from_port         = 20
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.my-luit-sg.id
 }
 
@@ -50,9 +161,7 @@ resource "aws_security_group_rule" "asg-egress-all" {
 resource "aws_security_group" "luit-alb-sg" {
   name        = "luit-alb-sg"
   description = "Allow all inbound HTTP traffic "
-
-  vpc_id = var.default-vpc_id
-
+  vpc_id      = aws_vpc.my_vpc.id
 }
 
 #rules for the ALB security group
@@ -61,7 +170,7 @@ resource "aws_security_group_rule" "alb-ingress-http" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.luit-alb-sg.id
 }
 
@@ -70,7 +179,7 @@ resource "aws_security_group_rule" "alb-ingress-https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.luit-alb-sg.id
 }
 
@@ -79,16 +188,16 @@ resource "aws_security_group_rule" "alb-ingress-ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.luit-alb-sg.id
 }
 
 resource "aws_security_group_rule" "alb-eress-all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.luit-alb-sg.id
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.luit-alb-sg.id
   source_security_group_id = aws_security_group.my-luit-sg.id
 }
 
@@ -114,11 +223,11 @@ resource "aws_launch_template" "luit-launch-template" {
 #Create an autoscaling group with the following configuration with security group only accepting traffic from the ALB:
 
 resource "aws_autoscaling_group" "luit-asg" {
-  availability_zones        = ["us-east-1a", "us-east-1b"]
   desired_capacity          = 2
   max_size                  = 5
   min_size                  = 2
   health_check_grace_period = 300
+  vpc_zone_identifier       = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   force_delete              = true
   launch_template {
     id      = aws_launch_template.luit-launch-template.id
@@ -132,7 +241,7 @@ resource "aws_alb_target_group" "luit-tg" {
   name             = "luit-tg"
   port             = 80
   protocol         = "HTTP"
-  vpc_id           = var.default-vpc_id
+  vpc_id           = aws_vpc.my_vpc.id
   protocol_version = "HTTP1"
   target_type      = "instance"
   health_check {
@@ -152,7 +261,7 @@ resource "aws_lb" "luit-alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.luit-alb-sg.id]
-  subnets            = [var.subnet-id-us-east-1a, var.subnet-id-us-east-1b]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   ip_address_type    = "ipv4"
 
   enable_deletion_protection = false
@@ -174,4 +283,3 @@ resource "aws_lb_listener" "luit-alb-listener" {
 output "luit-alb-dns" {
   value = aws_lb.luit-alb.dns_name
 }
-
